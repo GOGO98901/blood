e629c0c9a676b03b5be961a68d67f6517dfa3ae8
GOGO98901 <gogo98901@live.co.uk>
2016-09-01 12:17:10 +0100

    Added sprite sheet

--- src/me/roryclaasen/blood/graphics/sprite/Spritesheet.java
+++ src/me/roryclaasen/blood/graphics/sprite/Spritesheet.java
@@ -3,11 +3,11 @@ package me.roryclaasen.blood.graphics.sprite;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
-public class Sprite {
+public class Spritesheet {
 	private Image image;
 	private boolean failed = false;
 
-	public Sprite(String ref) {
+	public Spritesheet(String ref) {
 		try {
 			image = new Image(ref);
 		} catch (SlickException e) {
@@ -16,12 +16,17 @@ public class Sprite {
 		}
 	}
 
-	public Sprite(Image image) {
-		this.image = image;
-	}
-
 	public Image getImage() {
 		return image;
+	}
+
+	public Sprite getSprite(int x, int y) {
+		return getSprite(x, y, 64, 64);
+	}
+
+	public Sprite getSprite(int x, int y, int width, int height) {
+		if (failed) return null;
+		return new Sprite(image.getSubImage(x, y, width, height));
 	}
 
 	public boolean hasFailed() {
--- src/me/roryclaasen/blood/graphics/tile/Tiles.java
+++ src/me/roryclaasen/blood/graphics/tile/Tiles.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.newdawn.slick.Color;
 
 import me.roryclaasen.blood.graphics.sprite.Sprite;
+import me.roryclaasen.blood.graphics.sprite.Spritesheet;
 
 public class Tiles {
 	private static List<Tile> tiles = new ArrayList<Tile>();
@@ -13,6 +14,12 @@ public class Tiles {
 	static {
 		tiles.add(new TileGround(new Sprite("textures/tiles/tileGroundWhite.png"), new Color(255, 255, 255)));
 		tiles.add(new TileWall(new Sprite("textures/tiles/tileWallBlack.png"), new Color(0, 0, 0)));
+		
+		Spritesheet wallsBlack = new Spritesheet("textures/tiles/tilesetWallBlack.png");
+		tiles.add(new TileWall(wallsBlack.getSprite(0, 0), new Color(10, 10, 10)));
+		tiles.add(new TileWall(wallsBlack.getSprite(64, 0), new Color(20, 20, 20)));
+		tiles.add(new TileWall(wallsBlack.getSprite(64, 64), new Color(30, 30, 30)));
+		tiles.add(new TileWall(wallsBlack.getSprite(0, 64), new Color(40, 40, 40)));
 	}
 
 	public void addTile(Tile tile) {
